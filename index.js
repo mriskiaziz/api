16a7393de2ebe658dc1b86ed9a7c2892f11669bd
mriskiaziz <mriski.119140160@student.itera.ac.id>
2022-12-16 17:48:51 +0700

    upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const product = require("./api/product")
+
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use("/coba", product)
+
+app.listen(PORT, () => console.log(`Server running in port ${PORT}`));
